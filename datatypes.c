ea4eb16ca98aebe4eab8ed924bb41ae4146167a0
otanadzetsotne <otanadzetsotne@yahoo.com>
2021-10-07 02:09:28 +0400

    Spacing fix

--- datatypes.c
+++ datatypes.c
@@ -3,28 +3,27 @@
 
 void printf_type_size (char type_name[], unsigned long size)
 {
-printf("%17s:%4lu \n", type_name, size);
+    printf("%17s:%4lu \n", type_name, size);
 }
 
 
 int main ()
 {
+    // Print C predefined datatypes size
 
-// Print C predefined datatypes size
+    printf_type_size("Integer", sizeof(int));
+    printf_type_size("Unsigned", sizeof(short));
+    printf_type_size("Short", sizeof(short));
+    printf_type_size("Unsigned short", sizeof(unsigned short));
+    printf_type_size("Long", sizeof(long));
+    printf_type_size("Unsigned Long", sizeof(unsigned long));
 
-printf_type_size("Integer", sizeof(int));
-printf_type_size("Unsigned", sizeof(short));
-printf_type_size("Short", sizeof(short));
-printf_type_size("Unsigned short", sizeof(unsigned short));
-printf_type_size("Long", sizeof(long));
-printf_type_size("Unsigned Long", sizeof(unsigned long));
+    printf_type_size("Float", sizeof(float));
+    printf_type_size("Double", sizeof(double));
+    printf_type_size("Long double", sizeof(double));
 
-printf_type_size("Float", sizeof(float));
-printf_type_size("Double", sizeof(double));
-printf_type_size("Long double", sizeof(double));
+    printf_type_size("Char", sizeof(char));
 
-printf_type_size("Char", sizeof(char));
-
-printf_type_size("Array of 10 int", sizeof(int[10]));
-printf_type_size("String <Hello>", sizeof("Hello"));
+    printf_type_size("Array of 10 int", sizeof(int[10]));
+    printf_type_size("String <Hello>", sizeof("Hello"));
 }
